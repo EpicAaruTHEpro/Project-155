493761481a678184d3f2fb8f7c11991a37d65e62
EpicAaruTHEpro <Atomicoolives@gmail.com>
2022-03-07 13:01:07 -0500

    Created collision detection

---
+++ js/game.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("game-play", {
+    schema : {
+        elementId: {type: "string", default: "#coin1"}
+    },
+    update: function (){
+        this.isCollided(this.data.elementId)
+    },
+    tick: function (){      
+        
+    },
+    isCollided: function (elementId) {
+        const element = document.querySelector(elementId)
+        element.addEventListener("collide", e => {
+            if (elementId.includes("#coin")) {
+                console.log(elementId + "collided")
+            }
+
+            else if (elementId.includes("#bird")) {
+                console.log(elementId + "collided")
+            }
+        })
+    }
+    
+})
---
+++ js/rotation.js
@@ -0,0 +1,67 @@
+AFRAME.registerComponent("diver-rotation-reader", {
+    schema : {
+        speedOfRotation: {type: "number", default: 0},
+        speedOfAscent: {type: "number", default : 0}
+    },
+    init: function (){
+        window.addEventListener("keydown", (e) => {
+
+        //get the data from the attributes
+        this.data.speedOfRotation = this.el.getAttribute("rotation");      
+        this.data.speedOfAscent = this.el.getAttribute("position");
+
+        var diverRotation = this.data.speedOfRotation;      
+        var diverPosition = this.data.speedOfAscent;
+
+
+            if (e.key === "ArrowRight") {
+                if (diverRotation.x < 10) {
+
+                    diverRotation.x += 0.5
+                    this.el.setAttribute("rotation", diverRotation)
+                }
+            }
+
+            if (e.key === "ArrowLeft") {
+                if (diverRotation.x > -10) {
+
+                    diverRotation.x -= 0.5
+                    this.el.setAttribute("rotation", diverRotation)
+                }
+            }
+
+            if (e.key == "ArrowUp") {
+                if (diverRotation.z < 20) {
+                    diverRotation.z +=0.5
+                    this.el.setAttribute("rotation", diverRotation)
+                }
+
+                if (diverPosition.y < 2) {
+                    diverPosition.y += 0.01
+                    this.el.setAttribute("position", diverPosition)
+                }
+            }
+
+            if (e.key == "ArrowDown") {
+                if (diverRotation.z > -20) {
+                    diverRotation.z -=0.5
+                    this.el.setAttribute("rotation", diverRotation)
+                }
+
+                if (diverPosition.y > -2) {
+                    diverPosition.y -= 0.01
+                    this.el.setAttribute("position", diverPosition)
+                }
+            }
+            
+        })  
+    },
+    update: function (){
+
+    },
+    tick: function (){      
+        var mapRotation = this.el.getAttribute("rotation")
+        mapRotation.y += this.data.speedOfRotation
+        this.el.setAttribute("rotation", {x: mapRotation.x, y: mapRotation.y, z: mapRotation.z})
+    }
+})
